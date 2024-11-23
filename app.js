ad939fdd88f2aa838c4fab5a847eca78ac40b48f
bilalakmal-2004 <ghulambeast468@gmail.com>
2024-11-23 17:00:01 +0500

    New code added

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+let users = [];
+function User(username, password, email, gender, city) {
+  this.username = username;
+  this.password = password;
+  this.email = email;
+  this.gender = gender;
+  this.city = city;
+}
 function inValidEmail(e) {
   let inputElement = e.target;
   let spanElement = inputElement.nextElementSibling;
@@ -12,7 +20,6 @@ function inValidEmail(e) {
     return;
   }
   spanElement.style.display = "none";
-  return;
 }
 function inValidPassword(e) {
   let inputElement = e.target;
@@ -24,7 +31,6 @@ function inValidPassword(e) {
     return;
   }
   spanElement.style.display = "none";
-  return;
 }
 function inValidUserName(e) {
   let inputElement = e.target;
@@ -36,8 +42,63 @@ function inValidUserName(e) {
     return;
   }
   spanElement.style.display = "none";
-  return;
 }
 function checkOnSubmit(e) {
   e.preventDefault();
+
+  // Radio Button Code here
+  let radio = document.getElementsByName("gender");
+  let isRadioSelected = false;
+
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      // console.log(radio[i].value);
+      isRadioSelected = true;
+      break;
+    }
+  }
+
+  if (!isRadioSelected) {
+    alert("Must select a radio button!");
+  }
+  //  selcet code here
+  let selectButtonCities = document.getElementById("cities");
+  if (selectButtonCities.selectedIndex == 0) {
+    alert("Must select a city!");
+  } else {
+    // console.log(selectButtonCities.value);
+  }
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value;
+  const city = selectButtonCities.value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+
+  let emailExists = false;
+  let usernameExists = false;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email) {
+      emailExists = true;
+      break;
+    }
+    if (users[i].username === username) {
+      usernameExists = true;
+      break;
+    }
+  }
+  if (emailExists) {
+    alert("Email already exists!");
+    return;
+  }
+
+  if (usernameExists) {
+    alert("Username already exists!");
+    return;
+  }
+  const newUser = new User(username, password, email, gender.value, city);
+  users.push(newUser);
+
+  alert("User registered successfully!");
+
+  console.log(users);
 }
